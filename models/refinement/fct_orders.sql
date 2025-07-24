3f90cadf9146ba205c0848424c51d12538aae5ed
Alda Çadri <alda.cadri@hyperisland.se>
2025-07-24 18:38:49 +0000

    changed product_code to product_key

--- models/refinement/fct_orders.sql
+++ models/refinement/fct_orders.sql
@@ -5,7 +5,7 @@ with enriched as (
     survey_responseid      as user_key,
     order_date             as date_key,
     coalesce(final_fips, '00')      as state_key,     -- ‘00’ = unknown/digital
-    coalesce(product_code, 'UNKNOWN') as product_key, -- catch any missing ASINs
+    coalesce(product_key, 'UNKNOWN') as product_key, -- catch any missing ASINs
     quantity,
     unit_price,
     order_value
