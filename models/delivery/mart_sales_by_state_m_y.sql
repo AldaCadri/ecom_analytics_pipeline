1abd60be50e83b95cfe81291c90fe13ca3eb2f67
Alda Çadri <alda.cadri@hyperisland.se>
2025-07-28 08:44:37 +0000

    changed the code to get state name

--- models/delivery/mart_sales_by_state_m_y.sql
+++ models/delivery/mart_sales_by_state_m_y.sql
@@ -3,7 +3,7 @@
 select
   d.date_key           as month,         -- first of month from dim_date
   d.year,                             
-  s.state_fips          as state_key,    
+  s.state_name          as state_name,    
   count(*)               as orders_count,
   sum(f.order_value)     as total_revenue,
   avg(f.order_value)     as avg_order_value
@@ -14,7 +14,7 @@ join {{ ref('dim_state')  }}   s
   on f.state_key = s.state_fips
 
 -- default to last 3 years for dashboard performance
-where d.year >= date_part('year', current_date()) - 2  
+--where d.year >= date_part('year', current_date()) - 2  
 
 group by 1,2,3
 order by 2 desc, 1 desc, 3
